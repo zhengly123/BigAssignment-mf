8a55bbbc5fb035175bcb45fe7d4d0492ef31fd80
li-yc <liyichen8@126.com>
2016-11-27 17:19:42 +0800

    完成了底层和中间层，编译错误，无法调试
    第一层用的是没有旋转侧面函数时的笨办法，所以代码较长

--- BigAssignment-mf/BigAssignment-mf/main.cpp
+++ BigAssignment-mf/BigAssignment-mf/main.cpp
@@ -1,172 +1,396 @@
+  // created by lyc. level 1, level 2, level 3 has been finished.
 #include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <direct.h>
 #include "MagicCube.h"
 using namespace std;
-typedef pair<int, int> pii;
-MagicCube a, b;
-
-void simulation()//���������ģ��
+int lf[8]={1,4,3,2,1,4,3,2};  // lf = lateral face, [0] = green, [1] = orange, [2] = blue, [3] = red;
+MagicCube a, b, c;  // assume that cube is the cube that we are going to restore in function level 1 to 7
+bool bts(int a1, int a2, int a3)  // bts = big to small
 {
-	int t1, t2;
-	while (cin >> t1 >> t2)
-	{
-		b = a;
-		a.rotate(t1, t2);
-		a.output_compare(b, 1);
-	}
-	a.output_detailed();
-	//a.output_compare(b);
+    if (a1>a2 && a2>a3)
+        return 1;
+    return 0;
 }
-
-void check_rotate()//debug��ת
+bool b50(int a0)  // between 5 and 0
 {
-	b.set_different_number();
-	for (int i = 0; i < Face_Count; ++i)
-	{
-		a.set_different_number();
-		a.rotate(i, 0);
-		//a.output_check(b, 1);
-		//if (!a.accuracy_check()) puts("ERR");
-
-		a.set_different_number();
-		a.rotate(i, 1);
-		//a.output_check(b, 1);
-		//if (!a.accuracy_check()) puts("ERR");
-	}
+    if (5>a0 && a0>0)
+        return 1;
+    return 0;
 }
-
-void work()
+struct line
 {
-	//simulation();
-	pii c = a.query(0, 5);
-	printf("%d,%d", c.first, c.second);
-}
-//rel��¼����������������棬rel[0]�����ǰ����[1]����ĺ��
-const int rel[2][6] = { { -1,4,1,2,3,-1, },{ -1, 2, 3, 4, 1, -1 } };
-const int r[9] = { 2,1,1,2,-1,4,3,3,4 };//�ײ������
-void solve_bottom_cross_one()
+    int direct,time;
+};
+bool cb(MagicCube *cube, int face, int color)  // cb = check block.
 {
-	pii cur = a.query(0, 1);
-	int flag = 0;
-	if (cur == make_pair(0, 1)) return;
-	if (cur.first == 0)
-		for (int i = 0; i < 2; ++i)
-			a.rotate(r[cur.second], 0);
-	cur = a.query(0, 1);
-	if (0 < cur.first&&cur.first < 5)
-	{
-		if (cur.second == 1)
-		{
-			for (cur = a.query(0, 1); cur.first != 1; cur = a.query(0, 1)) a.rotate(5, 0);
-			a.rotate(1, 0);
-		}
-		cur = a.query(0, 1);
-		if (cur.second == 7) a.rotate(cur.first, 0);
-		cur = a.query(0, 1);
-		if (cur.second ==3||cur.second==5)
-		{
-			flag = (cur.second == 3);
-			a.rotate(rel[flag][cur.first], 0);
-			a.rotate(5, flag);
-			a.rotate(rel[flag][cur.first], 1);
-		}
-	}
-	for (cur = a.query(0, 1); cur.first == 5 && cur.second != 7; cur = a.query(0, 1))
-	{
-		a.rotate(5, 0);
-	}
-	if (cur.first == 5)
-		for (int i = 0; i < 2; ++i) a.rotate(1, 0);
+    if (cube->query(face,color) == make_pair(face,color))
+        return 1;
+    return 0;
 }
-
-void solve_bottom_cross()
+pii mp(int first, int second)  // mp is short for make_pair
 {
-	for (int i = 0; i < 4; ++i)
-	{
-		solve_bottom_cross_one();
-		if (a.query(0, 1) != make_pair(0, 1)) throw 1;//check
-		a.rotate_direction();
-	}
+    return make_pair(first, second);
 }
-
-void solve_first_level_one()
+pii cline(MagicCube precube, int face1, int pos1, int face2, int pos2)  // 用来检查是否能只旋转一面（不包括中间那一面）达到目标地点，pos = position, 输出第二块以顺时针按照(第一个输出）方向转（第二个输出）次能够到达第一块的位置，不能做到则输出1，0. 如果不用转，输出0，0.
 {
-#define C a.query(0,2)
-	int t1;
-	if (C == make_pair(0, 2)) return;
-	if (C.first == 0 || (C.first < 5 && C.second>5))
-	{
-		if (C.first == 0) t1 = rel[0][r[C.second]];
-		else
-		{
-			if (C.second == 8) t1 = rel[0][C.first];
-			else t1 = C.first;
-		}
-		a.rotate(t1, 0);
-		a.rotate(5, 0);
-		a.rotate(t1, 1);
-	}
-	if (C.first == 5)
-	{
-		while (C.second != 8) a.rotate(5, 0);
-		a.rotate(4, 0);
-		a.rotate(5, 1);
-		a.rotate(4, 1);
-	}
-	if (C.second == 2)
-	{
-		while (C.first != 1) a.rotate(5, 0);
-		a.rotate(5, 0);
-		a.rotate(4, 0);
-		a.rotate(5, 1);
-		a.rotate(4, 1);
-	}
-	if (C.second == 0)
-	{
-		while (C.first != 4) a.rotate(5, 0);
-		a.rotate(4, 0);
-		a.rotate(5, 0);
-		a.rotate(4, 1);
-	}
-#undef C
-}
-
-void solve_first_level()
+    int i0,j0;
+    MagicCube v = precube;
+    if (face1==face2 && pos1==pos2)
+        return make_pair(0, 0);
+    for (i0=0; i0<6; i0++)
+    {
+        v = precube;
+        for (j0=1; j0<=3; j0++)
+        {
+            v.rotate(i0,0);
+            if ((v.num[face1][pos1] == precube.num[face2][pos2]) && (v.color[face1][pos1] == precube.color[face2][pos2]))
+                return make_pair(i0,j0);
+        }
+    }
+    return make_pair(-1,0);
+}  // complete
+void vlateralch(MagicCube *aimc, bool clockwise)  // view lateral change, aimc = aim cube. If clockwise view lateral change looked from the top, green turn to orange. suspicious
 {
-	for (int i = 0; i < 4; ++i)
-	{
-		solve_first_level_one();
-		if (a.query(0, 2) != make_pair(0, 2)) throw 1;//check
-		a.rotate_direction();
-	}
-}
-
-void solve_second_level()
+    MagicCube a0;
+    int i0,j0=1,j1,k0;
+    for (k0=0; k0<4; k0++)
+    {
+        if (clockwise)
+            j1 = lf[3-k0];
+        else
+            j1 = lf[(k0+1)%4];
+        for (i0=0; i0<9; i0++)
+        {
+            a0.num[j0][i0] = aimc->num[j1][i0];
+            a0.color[j0][i0] = aimc->color[j1][i0];
+        }
+        j0 = j1;
+    }
+    aimc->rotate(0,!clockwise);
+    aimc->rotate(5,clockwise);
+    for (i0=0; i0<9; i0++)
+    {
+        a0.num[5][i0] = aimc->num[5][i0];
+        a0.color[5][i0] = aimc->color[5][i0];
+    }
+    for (i0=0; i0<9; i0++)
+    {
+        a0.num[0][i0] = aimc->num[0][i0];
+        a0.color[0][i0] = aimc->color[0][i0];
+    }
+    *aimc = a0;
+    cout << "view lateral change " << clockwise << ".\n";
+}  // preparative work completed
+void level1()  // 底层十字架
 {
-	for (int i = 0; i < 4; ++i)
-	{
-		//solve_second_level_one();
-		if (a.query(0, 2) != make_pair(0, 2)) throw 1;//check
-		a.rotate_direction();
-	}
-}
-
-void solve()
+    const pii notro = make_pair(0,0), unable = make_pair(1,0);  // notro = not rotate.
+    MagicCube vir = c;  // vir = virtural cube
+    int i,k,t,gpot;  //face represents the face going to rotate in each step. gpot = goal position on top
+    pii ppo,rodir,rodir2;  // ppo = present position. gpo = goal position. rodir = rotate direction.
+    bool finish;  // finish = finish one piece.
+    for (i=1; i<8; i+=2)
+    {
+        finish = false;
+        t = 0;
+        if ((i == 3) || (i == 5))
+            gpot = i;
+        else
+            gpot = 8-i;
+        while ((!finish) && (t<100))
+        {
+            ppo = c.query(0, i);
+            rodir = cline(c, 0, i, ppo.first, ppo.second);
+            if (rodir == notro)
+            {
+                finish = true;
+                break;
+            }
+            if (ppo.first == 0)  // 目标在底层，放到顶层
+            {
+                if (ppo.second == 1)
+                {
+                    c.rotate(1);
+                    c.rotate(1);
+                }
+                if (ppo.second == 3)
+                {
+                    c.rotate(2);
+                    c.rotate(2);
+                }
+                if (ppo.second == 5)
+                {
+                    c.rotate(4);
+                    c.rotate(4);
+                }
+                if (ppo.second == 7)
+                {
+                    c.rotate(3);
+                    c.rotate(3);
+                }
+                ppo = c.query(0, i);
+            }
+            rodir = cline(c, 0, i, ppo.first, ppo.second);
+            if ((ppo.first != 1) && (ppo.first != 5) && (rodir == unable))  // 目标在中间层，转到顶层
+            {
+                if (ppo.second == 1)  // 中间层的底部，转到中间层的中间
+                {
+                    c.rotate(ppo.second, 0);
+                    ppo = c.query(0, i);
+                }
+                if (ppo.second == 7)  // 中间层的上部，转到中间层的中间
+                {
+                    int bg,rotatetime=0;  // bg = goal
+                    if (i <= 3)
+                        bg = (i+1) / 2;
+                    else
+                        bg = (13-i) / 2;
+                    while (c.query(0, i).first != bg)
+                    {
+                        c.rotate(5,0);
+                        rotatetime += 1;
+                    }
+                    c.rotate(bg, 0);
+                    for (k=0; k<rotatetime; k++)
+                        c.rotate(5, 1);
+                }
+                if (ppo.second == 3 || ppo.second == 5)  // 中间层的中间，转到顶层
+                {
+                    for (k=2; k<=9; k++)
+                    {
+                        vir = c;
+                        vir.rotate(k/2, k%2);
+                        if (vir.query(0, i).first == 5)
+                        {
+                            c.rotate(k/2, k%2);
+                            break;
+                        }
+                    }
+                    c.rotate(5,0);
+                    c.rotate(k/2, (k+1)%2);
+                    ppo = c.query(0, i);
+                }
+            }
+            
+            if (ppo.first == 5)  // 目标在顶层，放到gpot
+            {
+                while (c.query(0,i).second != gpot)
+                    c.rotate(5,0);
+                ppo = c.query(0, i);
+            }
+            rodir = cline(c, 0, i, ppo.first, ppo.second);  // 当直接可以转到位置
+            if ((rodir != unable) && (rodir.first != 0))
+            {
+                for (k=0; k<rodir.first; k++)
+                    c.rotate(rodir.first,0);
+                finish = true;
+            }
+            t += 1;
+        }
+        if (t >= 100)
+            cout << "level 1 failed\n";
+    }
+}  // complete
+void l2for()  // level 2 formula.
 {
-	solve_bottom_cross();
-	solve_first_level();
-	a.output();
+    c.rotate(2,1);
+    c.rotate(5,1);
+    c.rotate(2,0);
+    c.rotate(5,0);
 }
-
+void level2()  // 底角
+{
+    int i,t=0,rotatetime;
+    pii ppo,rodir;
+    MagicCube vir = c;
+    for (i=0; i<9; i+=2)
+    {
+        if (i == 4)
+            i = 6;
+        while (c.query(0, 0) != make_pair(0, 0) && t < 400)
+        {
+            ppo = c.query(0, 0);  //
+            if (ppo == make_pair(0, 0))  // don't need rotate
+                break;
+            if ((ppo.first<5) && (ppo.second > 5))  // If ppo not on top level, move it to top;
+            {
+                rotatetime=0;
+                while (ppo != make_pair(1,6) && ppo != make_pair(2,8))  // 白色不在（0，0）及其相邻位置时
+                {
+                    rotatetime += 1;
+                    c.rotate(0,0);
+                    ppo = c.query(0,0);
+                }
+                l2for();
+                while (rotatetime != 4)
+                    c.rotate(0,0);
+                ppo = c.query(0,0);
+            }
+            if  (!(ppo.first<5) && (ppo.second > 5)) // ppo on top
+            {
+                while (c.query(0,0) != make_pair(5,6) && c.query(0,0) != make_pair(1,0) && c.query(0,0) != make_pair(2,2))
+                    c.rotate(5);  // 白色不在（5，6）及其相邻位置时
+                while (c.query(0,0) != make_pair(0,0))// 当转到顶上
+                    l2for();
+            }
+            t += 1;
+        }
+        vlateralch(&c,false);
+        if (t>=400)
+            cout << "level 2 failed";
+    }
+}  // level 2 completed
+void l3for(bool right)
+{
+    if (right)
+    {
+        c.rotate(5,0);
+        c.rotate(4,0);
+        c.rotate(5,1);
+        c.rotate(4,1);
+        c.rotate(5,1);
+        c.rotate(1,1);
+        c.rotate(5,0);
+        c.rotate(1,0);
+    }
+    else
+    {
+        c.rotate(5,1);
+        c.rotate(2,1);
+        c.rotate(5,0);
+        c.rotate(2,0);
+        c.rotate(5,0);
+        c.rotate(1,0);
+        c.rotate(5,1);
+        c.rotate(1,1);
+    }
+}
+bool isib(MagicCube cu, int num) // isib = is side block. 只能用于目标在正面, num = 3 or 5.
+{
+    if ((num==3 && b50(cu.color[1][3]) && b50(cu.color[2][5]))
+        || (num==5 && b50(cu.color[1][5]) && b50(cu.color[4][3])))
+        return 1;
+    return 0;
+}
+void level3()
+{
+    pii ppo;
+    int t=0,i,finish=0,j;
+    for (i=1; i<=4; i++)  // 把中间层全部移到顶上
+    {
+        if (isib(c,3) && c.color[2][5] != 2 && c.color[1][3] != 1)
+        {
+            while (b50(c.color[1][1]) && b50(c.color[5][7]))
+                c.rotate(5,0);
+            l3for(false);
+        }
+        vlateralch(&c, true);
+    }
+    while (finish < 4)
+    {
+        for (i=1; i<5; i++)
+            if (c.color[i][1] == i)
+            {
+                for (j=1; j<=i-1; j++)
+                    vlateralch(&c,false);
+                if (c.color[5][7] == 2)
+                    l3for(false);
+                if (c.color[5][7] == 4)
+                    l3for(true);
+                if (c.color[5][7] != 2 && c.color[5][7] != 4)  // test
+                    cout << "level 3 aalgorithm wrong.\n";
+                    l3for(true);
+                for (j=1; j<=i-1; j++)
+                    vlateralch(&c,true);
+                finish += 1;
+            }
+        c.rotate(5,0);
+    }
+    if (t>=400)
+        cout << "level 3 failed.\n";
+}  // level 3 completed
+void l4for1()  // f,u,r,ui,ri,fi
+{
+    c.rotate(1,0);
+    c.rotate(5,0);
+    c.rotate(4,0);
+    c.rotate(5,1);
+    c.rotate(4,1);
+    c.rotate(1,1);
+}
+void l4for2()  // f,r,u,ri,ui,fi
+{
+    c.rotate(1,0);
+    c.rotate(4,0);
+    c.rotate(5,0);
+    c.rotate(4,1);
+    c.rotate(5,1);
+    c.rotate(1,1);
+}
+int yline(MagicCube cube)  // yellow line. If there is yellow blocks in (5,1) (5,7), return 1. The other kind of yellow line return 2. If no yellow line, return 0.
+{
+    if (cube.color[5][1] == 5 && cube.color[5][7] == 5)
+        return 1;
+    if (cube.color[5][3] == 5 && cube.color[5][5] == 5)
+        return 2;
+    return 0;
+}
+int ycor(MagicCube cube)  // yellow corner. If (5,1)(5,3) return 1. If (5,3)(5,7) return 2
+{
+    if (cube.color[5][1] == 5 && cube.color[5][3] == 5)
+        return 1;
+    if ((cube.color[5][3] == 5 && cube.color[5][7] == 5) ||
+        (cube.color[5][7] == 5 && cube.color[5][5] == 5) ||
+        (cube.color[5][5] == 5 && cube.color[5][1] == 5))
+        return 2;
+    return 0;
+}
+void l4for3()  // r,b,ri,b,r,b,b,ri
+{
+    c.rotate(4,0);
+    c.rotate(5,0);
+    c.rotate(4,1);
+    c.rotate(5,0);
+    c.rotate(4,0);
+    c.rotate(5,0);
+    c.rotate(5,0);
+    c.rotate(4,1);
+}
+void level4cross()
+{
+    if (!yline(c) && !ycor(c))
+    {
+        l4for1();
+        level4cross();
+    }
+    else
+    {
+        if (yline(c) == 1)
+            c.rotate(5,0);
+        if (yline(c) == 2)
+        {
+            l4for2();
+            return;
+        }
+        if (ycor(c))
+        {
+            while (ycor(c) != 1)
+                c.rotate(5,0);
+            l4for1();
+            return;
+        }
+    }
+}
+void level4()
+{
+    level4cross();
+    
+}
 int main()
 {
-	//freopen("mf.in", "r", stdin);
-	//freopen("mf.out", "w", stdout);
-	a.randomize();
-	solve();
-	puts("GET TO GETCHAR()");
-	getchar(); getchar();
-	return 0;
+    c.randomize(200);
+    level1();
+    level2();
+    c.output();
+    
+    
+    return 0;
 }
